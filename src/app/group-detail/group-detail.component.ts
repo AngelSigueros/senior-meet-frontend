09f2a11402a13329230f7ad60e170329d901766e
yesseniach <148784983+yesseniach@users.noreply.github.com>
2024-03-04 19:18:49 +0100

    group

---
+++ src/app/group-detail/group-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './group-detail.component.html',
+  styleUrl: './group-detail.component.css'
+})
+export class GroupDetailComponent {
+
+}
---
+++ src/app/group-list/group-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './group-list.component.html',
+  styleUrl: './group-list.component.css'
+})
+export class GroupListComponent {
+
+}
