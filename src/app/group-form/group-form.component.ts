a579db17c38d9bbe3bf850bf5e0c87c03a859e78
yesseniach <148784983+yesseniach@users.noreply.github.com>
2024-03-11 16:57:04 +0100

    group form

---
+++ src/app/group-form/group-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-group-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './group-form.component.html',
+  styleUrl: './group-form.component.css'
+})
+export class GroupFormComponent {
+
+}
--- src/app/group-list/group-list.component.ts
+++ src/app/group-list/group-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Group } from '../models/group.model';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-group-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterLink],
   templateUrl: './group-list.component.html',
   styleUrl: './group-list.component.css'
 })
