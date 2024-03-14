9521ec48824779346cbcf500b32bea398c489385
AngelSigueros <pickassblog@gmail.com>
2024-03-14 21:13:05 +0100

    quitar componentes no utilizados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeWallComponent } from './home-wall/home-wall.component';
 import { AdminComponent } from './admin/admin.component';
-import { CreateProfileComponent } from './create-profile/create-profile.component';
-import { DeleteProfileComponent } from './delete-profile/delete-profile.component';
-import { EditeProfileComponent } from './edite-profile/edite-profile.component';
 import { LoginComponent } from './login/login.component';
 import { PostComponent } from './post/post.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { SavedComponent } from './saved/saved.component';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { GroupListComponent } from './group-list/group-list.component';
@@ -37,18 +33,6 @@ export const routes: Routes = [
   {
     path: 'admin',
     component: AdminComponent
-  },
-  {
-    path: 'create-profile',
-    component: CreateProfileComponent
-  },
-  {
-    path: 'delete-profile',
-    component: DeleteProfileComponent
-  },
-  {
-    path: 'edite-profile',
-    component: EditeProfileComponent
   },
   {
     path: 'groups',
@@ -85,10 +69,6 @@ export const routes: Routes = [
   {
     path: 'user-profile/:id/detail',
     component: UserProfileComponent
-  },
-  {
-    path: 'saved',
-    component: SavedComponent
   },
   {
     path: 'users',
