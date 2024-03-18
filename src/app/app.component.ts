631140de26ed31f861223c4e2b84c8b644c32d40
AngelSigueros <pickassblog@gmail.com>
2024-03-18 13:00:25 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,5 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Test Senior Meet';
+  title = 'Test Senior Meet Front';
 }
