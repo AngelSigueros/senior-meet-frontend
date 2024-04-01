34ebafe1b6b7b6de999b02dda9647387b807eabd
fran-eliot <ramirez.martin.francisco@gmail.com>
2024-04-01 13:38:49 +0200

    Cambio de la estructura de modelo de Post añadiendo campos

--- src/app/models/post.model.ts
+++ src/app/models/post.model.ts
@@ -1,5 +1,14 @@
+import { Group } from "./group.model";
+import { Interaction } from "./interaction.model";
+import { User } from "./user.model";
+
 export interface Post {
     id: number;
     title: string;
     content: string;
+    photoUrl: string;
+    videoUrl: string;
+    group: Group;
+    user: User;
+    interactions: Interaction[];
 }
