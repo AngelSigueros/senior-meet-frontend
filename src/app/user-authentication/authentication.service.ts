41552fb13e664dacd218783ad6167eb44511e704
AngelSigueros <pickassblog@gmail.com>
2024-04-04 20:47:00 +0200

    jwt

--- src/app/user-authentication/authentication.service.ts
+++ src/app/user-authentication/authentication.service.ts
@@ -1,17 +1,50 @@
 import { Injectable } from '@angular/core';
+import { jwtDecode } from 'jwt-decode';
 import { BehaviorSubject } from 'rxjs';
+import { DecodedToken } from './decoded-token.dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  isLoggedIn = new BehaviorSubject<boolean>(false);
+  isLoggedIn = new BehaviorSubject<boolean>(this.existToken());
+  userEmail =  new BehaviorSubject<string>(this.getUserEmail());
+  isAdmin =  new BehaviorSubject<boolean>(this.getIsAdmin());
 
-  constructor() { }
+  constructor() {}
+
+  getIsAdmin() {
+    const token = localStorage.getItem("jwt_token");
+    if (!token) return false;
+    console.log(jwtDecode(token));
+    const decodedToken = jwtDecode(token) as DecodedToken;
+    return decodedToken.role === 'admin';
+  }
+
+  getUserEmail(): string {
+    const token = localStorage.getItem("jwt_token");
+    if (!token) return '';
+    console.log(jwtDecode(token));
+    const decodedToken = jwtDecode(token) as DecodedToken;
+    return decodedToken.email;
+  }
+
+  existToken() {
+    return localStorage.getItem("jwt_token") != null;
+  }
 
   saveToken(token: string) {
     localStorage.setItem("jwt_token", token);
     this.isLoggedIn.next(true);
+    this.userEmail.next(this.getUserEmail());
+    this.isAdmin.next(this.getIsAdmin());
+  }
+
+  removeToken() {
+    localStorage.removeItem("jwt_token");
+    this.isLoggedIn.next(false);
+    this.userEmail.next('');
+    this.isAdmin.next(false);
   }
 }
