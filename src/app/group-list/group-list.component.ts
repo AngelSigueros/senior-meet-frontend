6dbbe6da5422b8e97b84d24968a46de2292f38f7
yesseniach <148784983+yesseniach@users.noreply.github.com>
2024-04-04 20:32:21 +0200

    again

--- src/app/group-list/group-list.component.ts
+++ src/app/group-list/group-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Group } from '../models/group.model';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-group-list',
@@ -12,15 +12,21 @@ import { RouterLink } from '@angular/router';
 })
 export class GroupListComponent implements OnInit{
 
+
   groups: Group[] = []
 
-  constructor (private http: HttpClient){}
+  constructor (private http: HttpClient, private router: Router){}
 
   ngOnInit(): void {
     console.log('GroupListComponent');
 
     this.http.get<Group[]>("http://localhost:8080/groups").subscribe(g=>this.groups=g);
   }
-
+  addGroupToUser(group: Group) {
+    this.http.get('http://localhost:8080/user/add-group/' + group.id).subscribe(user => {
+      // navegar al detalle del grupo
+      this.router.navigate(['/groups', group.id, 'detail']);
+    });
+    }
 
 }
