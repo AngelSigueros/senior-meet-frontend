4367e97c3a5ed0129ec3d551aa78fedbed49869d
yesseniach <148784983+yesseniach@users.noreply.github.com>
2024-04-08 20:08:06 +0200

    Intento de crear grupo nuevo

--- src/app/group-form/group-form.component.ts
+++ src/app/group-form/group-form.component.ts
@@ -1,52 +1,95 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Group } from '../models/group.model';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-group-form',
   standalone: true,
-  imports: [ReactiveFormsModule, HttpClientModule],
+  imports: [ReactiveFormsModule, HttpClientModule, RouterLink],
   templateUrl: './group-form.component.html',
   styleUrl: './group-form.component.css'
 })
-export class GroupFormComponent {
-  groupForm = this.fb.group({
-    id: [0],
-    title: [''],
-    description: [],
-    photoUrl: [],
-    rules: [],
+export class GroupFormComponent implements OnInit{
+  groupForm = new FormGroup({
+    id: new FormControl(0),
+    title: new FormControl(''),
+    description: new FormControl(''),
+    photoUrl: new FormControl(''),
+    rules:  new FormControl('')
+});
+photoFile: File | undefined;
+photoPreview: string | undefined;
+group: Group| undefined;
+isUpdate: boolean = false;
 
-  });
+  
 
-  constructor(private fb: FormBuilder, private httpClient: HttpClient) {}
 
-  save() {
+  constructor(
+    private httpClient: HttpClient,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      const id = params['id'];
+      if(!id) return;
+      
 
-    console.log("Guardando grupo");
+      this.httpClient.get<Group>('http://localhost:8080/groups/' + id).subscribe(group => {
+        this.groupForm.reset(group);
+        this.isUpdate = true;
+        this.group = group;
 
-    // Extraer los valores de cada input escritos por el usuario
-    const id = this.groupForm.get('id')?.value ?? 0;
-    const title = this.groupForm.get('title')?.value ?? 'titulo por defecto';
-    const description= this.groupForm.get('description')?.value ??'description';
-    const photoUrl= this.groupForm.get('photoUrl')?.value ?? 'foto';
-    const rules= this.groupForm.get('rules')?.value ?? 'Reglas';
-
-    // Crear un objeto utilizando los valores extraídos
-    const groupToSave: Group = {
-      id: id,
-      title: title,
-      description: description,
-      photoUrl: photoUrl,
-      rules: rules,
-      users: []
-    }
-    console.log(groupToSave);
+      });
+    });
   }
+  
+  onFileChange(event: Event) {
+    let target = event.target as HTMLInputElement; // este target es el input de tipo file donde se carga el archivo
+
+    if(target.files === null || target.files.length == 0){
+      return; // no se procesa ningún archivo
+    }
+
+    this.photoFile = target.files[0];
+  }
+
+  save(){
+    let formData = new FormData();
+    formData.append('photoUrl', this.groupForm.get('photoUrl')?.value ?? '');
+    formData.append('id', this.groupForm.get('id')?.value?.toString() ?? '0');
+    formData.append('title', this.groupForm.get('title')?.value ?? '');
+    formData.append('description', this.groupForm.get('description')?.value ?? '');
+    formData.append('rules', this.groupForm.get('rules')?.value ?? '');
+
+    console.log(formData)
+    if(this.photoFile) {
+      formData.append("photo", this.photoFile);
+    }
+
+    if (this.isUpdate) {
+      this.httpClient.put<Group>('http://localhost:8080/groups/' + this.group?.id, formData)
+    .subscribe(group => this.navigateToList());
+    } else {
+    this.httpClient.post<Group>('http://localhost:8080/groups', formData)
+    .subscribe(group=> this.navigateToList());
+     }
+  }
+
+  private navigateToList() {
+    this.router.navigate(['/groups']);
+  }    
 }
 
 
 
+
+
+
   
+
+ 
 
