ea06f44e05c484a8cfd423a8ff8764320ddd87ba
AngelSigueros <pickassblog@gmail.com>
2024-04-09 10:51:06 +0200

    funcionalidad user-login

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,13 +1,12 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user.model';
-import { Router, RouterLink } from '@angular/router';
 import { AuthenticationService } from '../user-authentication/authentication.service';
 
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [HttpClientModule, RouterLink],
+  imports: [HttpClientModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
@@ -18,23 +17,17 @@ export class UserListComponent implements OnInit {
   userEmail = '';
   isAdmin = false;
 
-  constructor(private authService: AuthenticationService,
-    private http: HttpClient,
-    private router: Router) {
-    this.authService.isLoggedIn.subscribe(isLoggedIn=>this.isLoggedIn=isLoggedIn);
-    this.authService.userEmail.subscribe(userEmail=>this.userEmail=userEmail);
+  constructor(private http: HttpClient,
+              private authService: AuthenticationService) { 
     this.authService.isAdmin.subscribe(isAdmin=>this.isAdmin=isAdmin);
-  }
+   }
 
   ngOnInit(): void {
-    console.log('UserListComponent');
+    console.log('UserListComponent - ' + this.isAdmin);
 
     this.http.get<User[]>("http://localhost:8080/user").subscribe(u=>this.users=u);
   }
 
-  logout() {
-    this.authService.removeToken();
-    this.router.navigate(['/user-login']);
-  }
+
 
 }
