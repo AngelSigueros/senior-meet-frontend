21ce0ee0c213b1c01df4ff0e38dc8b719c85aca8
fran-eliot <ramirez.martin.francisco@gmail.com>
2024-04-13 20:45:06 +0200

    Funcion añadir comentario

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -4,11 +4,12 @@ import { Post } from '../models/post.model';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Interaction } from '../models/interaction.model';
 import { Comment } from '../models/comment.model';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-post-detail',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule],
   templateUrl: './post-detail.component.html',
   styleUrl: './post-detail.component.css'
 })
@@ -19,7 +20,14 @@ export class PostDetailComponent implements OnInit{
   comments: Comment[]=[]
   interactions: Interaction[]= []
 
-  constructor (private http: HttpClient, private activatedRoute: ActivatedRoute){}
+  commentForm = this.fb.group({
+    id: [0],
+    content: ['', Validators.required],
+    user: new FormControl(),
+    date: new Date()
+  });
+
+  constructor (private fb:FormBuilder, private http: HttpClient, private activatedRoute: ActivatedRoute){}
 
   ngOnInit(): void {
     console.log('PostDetailComponent');
@@ -34,5 +42,22 @@ export class PostDetailComponent implements OnInit{
     this.http.get<Interaction[]>("http://localhost:8080/post/"+params['id']+"/interactions").subscribe(i=>this.interactions=i);
   });
 
+  }
+
+  save(){
+    const id = this.commentForm.get('id')?.value?? 0;
+    const content = this.commentForm.get('content')?.value?? 'Contenido comentario';
+    const user = this.commentForm.get('user')?.value;
+    const date = this.commentForm.get('date')?.value?? new Date();
+
+    const commentToSave: Comment = {
+      id: id,
+      content: content,
+      user: user,
+      date: date
+    }
+
+    const url = 'http://localhost:8080/comment';
+    this.http.post<Post>(url, commentToSave).subscribe(comment => console.log(comment));
   }
 }
