b3c4ef276033bf279dd6939f2d6f2f08d7631e95
lilirubim <148784760+lilirubim@users.noreply.github.com>
2024-04-15 20:05:21 +0200

    homw-wall

--- src/app/home-wall/home-wall.component.ts
+++ src/app/home-wall/home-wall.component.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 import { Group } from '../models/group.model';
 import { RouterLink } from '@angular/router';
+import { Hobby } from '../models/hobby.model';
+import { Post } from '../models/post.model';
+import { User } from '../models/user.model';
+import { Comment } from '../models/comment.model';
 
 @Component({
   selector: 'app-home-wall',
@@ -13,7 +17,12 @@ import { RouterLink } from '@angular/router';
 })
 export class HomeWallComponent {
   images = [700, 533, 807, 124].map((n) => `https://picsum.photos/id/${n}/900/500`);
-  groups: Group[] = []
+  groups: Group[] = [];
+  hobbies: Hobby[]=[];
+  posts: Post[]=[];
+  comments: Comment[]=[];
+  users: User[]=[];
+  
 
 	constructor(config: NgbCarouselConfig, private http: HttpClient) {
 		// customize default values of carousels used by this component tree
